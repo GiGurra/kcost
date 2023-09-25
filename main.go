1573c1c9412ffba7173f3bae195c7cc476c43b31
Johan Kjölhede <gigurra@gmail.com>
2023-09-25 22:35:33 +0200

    wip

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"os"
 	"os/exec"
+	"slices"
 )
 
 func main() {
@@ -45,10 +46,33 @@ func main() {
 		nodeLkup[node.Name()] = node
 	}
 
+	podPriceLkup := make(map[string]float64)
+	for _, pod := range pods {
+		podPriceLkup[pod.Name()] = podPrice(pod, nodeLkup[pod.NodeName()], prices.GKE.Autopilot)
+	}
+
+	slices.SortFunc(pods, func(a, b model.Pod) int {
+		return int(podPriceLkup[a.Name()] - podPriceLkup[b.Name()])
+	})
+
+	slog.Info("-------------------------")
+	slog.Info("-------------------------")
+	slog.Info("      PRICES PER POD	 ")
+	slog.Info("-------------------------")
 	for _, pod := range pods {
 		node := nodeLkup[pod.NodeName()]
 		slog.Info(fmt.Sprintf("Pod %s: spot=%v, cpu=%f, memory=%f => price=%f\n", pod.Name(), node.IsSpotNode(), pod.CPURequestCores(), pod.MemoryRequestGB(), podPrice(pod, node, prices.GKE.Autopilot)))
 	}
+
+	slog.Info("-------------------------")
+	slog.Info("-------------------------")
+	slog.Info("      PRICE FOR NS       ")
+	slog.Info("-------------------------")
+	priceSum := 0.0
+	for _, pod := range pods {
+		priceSum += podPriceLkup[pod.Name()]
+	}
+	slog.Info(fmt.Sprintf("Total price: %f\n", priceSum))
 }
 
 func podPrice(
